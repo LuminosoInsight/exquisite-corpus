170a7c8442f5fe7d702c807368d518c66e23c18d
Rob Speer <rob@luminoso.com>
2018-05-31 11:26:32 -0400

    Reorganize when 'lossy_tokenize' happens to apply it consistently
    This sets up a consistent flow to avoid sources that have gone through
    different tokenization steps:
    
    - `xc count` uses `tokenize`, and takes files from tokenized to
      messy-counts
    - `xc recount` uses `lossy_tokenize`, and takes files from messy-counts
      to counts
    - All sources go through `xc recount` eventually
    
    Additionally, when recounting, instead of using our own PUNCT_RE to try
    to exclude non-word tokens, we use TOKEN_RE from wordfreq.

--- exquisite_corpus/tokens.py
+++ exquisite_corpus/tokens.py
@@ -1,4 +1,4 @@
-from wordfreq.tokens import tokenize, lossy_tokenize
+from wordfreq.tokens import tokenize
 from ftfy import fix_text
 from ftfy.fixes import unescape_html, fix_surrogates
 import regex
@@ -77,7 +77,7 @@ def tokenize_file(infile, outfile, language, check_language=False, punctuation=F
         else:
             # Run only specific quick fixes from ftfy
             line = fix_surrogates(unescape_html(line.rstrip()))
-        tokens = lossy_tokenize(line, language, include_punctuation=punctuation, external_wordlist=True)
+        tokens = tokenize(line, language, include_punctuation=punctuation, external_wordlist=True)
         checked_lang = None
         if check_language:
             checked_lang, _confident = cld2_detect_language(line.rstrip())
