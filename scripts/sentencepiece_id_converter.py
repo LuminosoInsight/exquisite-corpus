5cbc6532fde9de929eaa357b46eaf6ced7c00343
Robyn Speer <rspeer@luminoso.com>
2019-05-31 12:16:13 -0400

    Reinstate the PyTorch language model
    This reverts commit 8f54e04d15cc7b89e1cb8c76fe17d55ad7256fb5.

---
+++ scripts/sentencepiece_id_converter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+Utility script to read a text file containing space-delimited sequences of
+SentencePiece encoding ids (integers), and convert it to an .npy file.
+The input must have the same number of ids on every line, and the output
+will store a 2D array with that many columns (and one row for each line of
+the input).  The input file (base) name (minus any extension) must have the
+form 'length_n' where n is number of ids per line.
+"""
+
+import numpy as np
+import os
+import re
+import sys
+
+
+def main(input, output):
+    # First parse the input file name to find the width of the data.
+    basename = os.path.splitext(os.path.basename(input))[0]
+    match = re.match(r"length_(\d+)$", basename)
+    if match is None:
+        raise ValueError("Invalid file name {}.".format(input))
+    else:
+        numeral = match.group(1)
+        try:
+            n_fields = int(numeral)
+        except ValueError:
+            raise ValueError(
+                "Invalid numeral {} in file name {}.".format(numeral, input)
+            )
+
+    # Now read the input file to establish its length and check the width.
+    n_lines = 0
+    with open(input, "rt", encoding="utf-8") as fp:
+        for line in fp:
+            n_lines += 1
+            if n_fields != len(line.split()):
+                raise ValueError(
+                    "Input file {} has invalid width at line {}.".format(input, n_lines)
+                )
+
+    # Allocate a result array.
+    result = np.empty((n_lines, n_fields), dtype=np.int16)
+
+    # Re-read the input, and populate the result.
+    with open(input, "rt", encoding="utf-8") as fp:
+        for i_line, line in enumerate(fp):
+            result[i_line] = np.array([int(i) for i in line.split()])
+
+    # Write the output.
+    np.save(file=output, arr=result)
+
+
+if __name__ == "__main__":
+    main(input=sys.argv[1], output=sys.argv[2])
---
+++ scripts/slice_sentencepiece_ids.py
@@ -0,0 +1,145 @@
+"""
+Utility to read space-delimited tokens on lines from stdin, and distribute
+the lines to a fixed number of output files based on length (number of tokens),
+splitting up any input lines longer than the fixed maximum length.
+"""
+
+import click
+import numpy as np
+import sentencepiece as spm
+import sys
+
+
+class Writer:
+    """
+    Context manager handling opening, closing, and writing to a fixed-size
+    set of output files.
+    """
+
+    def __init__(self, file_prefix, max_length):
+        self.out_files = []
+        for length in range(max_length + 1):
+            path = "{}{}.txt".format(file_prefix, length)
+            out_file = open(path, "wt", encoding="utf-8")
+            self.out_files.append(out_file)
+
+    def write(self, chunk):
+        self.out_files[len(chunk)].write(" ".join(chunk) + "\n")
+
+    def close(self):
+        for out_file in self.out_files:
+            out_file.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+
+class ChunkMaker:
+    """
+    Provides a method for generating bounded-size chunks of long input lists.
+    """
+
+    def __init__(self, min_chunk_length, max_chunk_length, seed=101):
+        self.min_chunk_length = min_chunk_length
+        self.max_chunk_length = max_chunk_length
+        self.rng = np.random.RandomState(seed=seed)
+        self.length_bound = np.max(
+            [2 * self.min_chunk_length, self.max_chunk_length + 1]
+        )
+
+    def make_chunks(self, original_chunk):
+        # Split big chunks into smaller ones, yielding chunks that satisfy the
+        # minimum and maximum size constraints, until there is nothing left to
+        # split.  Chunks shorter than twice the minimum chunk size must not be
+        # split; if they are longer than the maximum size (which won't happen
+        # if the maximum size is at least twice the minimum, minus one), we
+        # must discard part of the chunk (we choose to discard the tail).  Other
+        # chunks no longer than the maximum size will not be split (so we favor
+        # long chunks).  The remaining chunks are split into two pieces each
+        # at least of the minimum size.
+        chunks = [original_chunk]
+        while len(chunks) > 0:
+            chunk = chunks.pop()
+            length = len(chunk)
+            if length < self.length_bound:
+                yield chunk[: self.max_chunk_length]
+            else:
+                split = self.rng.randint(
+                    self.min_chunk_length, length - self.min_chunk_length + 1
+                )
+                chunks.append(chunk[:split])
+                chunks.append(chunk[split:])
+
+
+@click.command()
+@click.option(
+    "--spm-model",
+    type=click.Path(exists=True, dir_okay=False, path_type=str),
+    help="Location of trained SentencePiece model that produced the ids.",
+)
+@click.option(
+    "--file-prefix",
+    help=(
+        "Prefix for output file paths.  E.g. if this argument is 'foo' "
+        "then output files foo0.txt, foo1.txt, ... will be written."
+    ),
+)
+@click.option(
+    "--max-length",
+    type=click.INT,
+    help="Maximum output length.  Inputs longer than this will be split into pieces.",
+)
+@click.option(
+    "--min-chunk-length",
+    type=click.INT,
+    help="Minimum size of pieces into which to split long input lines.",
+)
+@click.option(
+    "--max-chunk-length",
+    type=click.INT,
+    help="Maximum size of pieces into which to split long input lines.",
+)
+@click.option(
+    "--random-seed",
+    type=click.INT,
+    default=101,
+    help="Seed for random generator (used to pick piece sizes).",
+)
+def slice_sentencepiece_ids(
+    spm_model,
+    file_prefix,
+    max_length,
+    min_chunk_length,
+    max_chunk_length,
+    random_seed=101,
+):
+    """
+    Read space-delimited text lines from stdin and write the lines
+    to output files corresponding to the length (number of space-
+    delimited tokens) of each line.  Long lines will be split into
+    pieces before being written.
+    """
+    assert 1 <= min_chunk_length <= max_chunk_length <= max_length
+    chunker = ChunkMaker(min_chunk_length, max_chunk_length, seed=random_seed)
+    sp = spm.SentencePieceProcessor()
+    sp.Load(spm_model)
+    start_ids = [str(sp["<s>"])]
+    end_ids = [str(sp["</s>"])]
+    with Writer(file_prefix, max_length) as writer:
+        for line in map(str.rstrip, sys.stdin):
+            tokens = line.split()
+            if len(tokens) < 1:
+                continue
+            tokens = start_ids + tokens + end_ids
+            if len(tokens) <= max_length:
+                writer.write(tokens)
+            else:
+                for chunk in chunker.make_chunks(tokens):
+                    writer.write(chunk)
+
+
+if __name__ == "__main__":
+    slice_sentencepiece_ids()
