d98be4fc5f288379552034b2a0ac0a6941005bb7
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-04 16:39:56 -0400

    Set a name for quick_run function

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,18 +5,18 @@ from shutil import copytree, rmtree
 
 
 def pytest_addoption(parser):
-    parser.addoption('--quick', action='store_true', help='Use existing input '
-                                                              'files in tests/data')
+    parser.addoption('--quick', action='store_true', help='Use existing input files in'
+                                                          ' tests/data')
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session', name='is_quick_run')
 def quick_run(pytestconfig):
     return pytestconfig.option.quick
 
 
 @pytest.fixture(scope='session')
-def setup_input_files(quick_run):
-    if not quick_run:
+def setup_input_files(is_quick_run):
+    if not is_quick_run:
         rmtree('tests/data', ignore_errors=True)
         copytree('tests/input_data/', 'tests/data/')
 
