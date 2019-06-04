68ea13c019d147f2a45324944b75e468c9946996
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-04 16:27:37 -0400

    Abstract directory names

--- tests/test_wordfreq_build.py
+++ tests/test_wordfreq_build.py
@@ -7,28 +7,41 @@ from tests.testing_utils import (
     run_snakemake,
 )
 
+RESULT_EXTRACTED = 'tests/data/extracted'
+RESULT_TOKENIZED = 'tests/data/tokenized'
+RESULT_MESSY_COUNTS = 'tests/data/messy-counts'
+RESULT_COUNTS = 'tests/data/counts'
+RESULT_FREQS = 'tests/data/freqs'
+RESULT_WORDFREQ = 'tests/data/wordfreq'
+REFERENCE_WORDFREQ = 'tests/reference/wordfreq'
+REFERENCE_TOKENIZED = 'tests/reference/tokenized'
+REFERENCE_EXTRACTED = 'tests/reference/extracted'
+REFERENCE_FREQS = 'tests/reference/freqs'
+REFERENCE_COUNTS = 'tests/reference/counts'
+REFERENCE_MESSY_COUNTS = 'tests/reference/messy-counts'
+
 
 @pytest.fixture(scope='session')
 def run_build(test_env_variables, setup_input_files):
     run_snakemake(test_env_variables)
     yield
-    rmtree('tests/data/extracted')
-    rmtree('tests/data/tokenized')
-    rmtree('tests/data/counts')
-    rmtree('tests/data/messy-counts')
-    rmtree('tests/data/freqs')
-    rmtree('tests/data/wordfreq')
+    rmtree(RESULT_EXTRACTED)
+    rmtree(RESULT_TOKENIZED)
+    rmtree(RESULT_COUNTS)
+    rmtree(RESULT_MESSY_COUNTS)
+    rmtree(RESULT_FREQS)
+    rmtree(RESULT_WORDFREQ)
 
 
 @pytest.mark.parametrize(
     'result, reference',
     [
-        ('tests/data/extracted', 'tests/reference/extracted'),
-        ('tests/data/tokenized', 'tests/reference/tokenized'),
-        ('tests/data/messy-counts', 'tests/reference/messy-counts'),
-        ('tests/data/counts', 'tests/reference/counts'),
-        ('tests/data/freqs', 'tests/reference/freqs'),
-        ('tests/data/wordfreq', 'tests/reference/wordfreq'),
+        (RESULT_EXTRACTED, REFERENCE_EXTRACTED),
+        (RESULT_TOKENIZED, REFERENCE_TOKENIZED),
+        (RESULT_MESSY_COUNTS, REFERENCE_MESSY_COUNTS),
+        (RESULT_COUNTS, REFERENCE_COUNTS),
+        (RESULT_FREQS, REFERENCE_FREQS),
+        (RESULT_WORDFREQ, REFERENCE_WORDFREQ),
     ],
 )
 def test_text_result_same_as_reference(run_build, result, reference):
@@ -38,9 +51,9 @@ def test_text_result_same_as_reference(run_build, result, reference):
 @pytest.mark.parametrize(
     'result, reference',
     [
-        ('tests/data/extracted', 'tests/reference/extracted'),
-        ('tests/data/tokenized', 'tests/reference/tokenized'),
-        ('tests/data/wordfreq', 'tests/reference/wordfreq'),
+        (RESULT_EXTRACTED, REFERENCE_EXTRACTED),
+        (RESULT_TOKENIZED, REFERENCE_TOKENIZED),
+        (RESULT_WORDFREQ, REFERENCE_WORDFREQ),
     ],
 )
 def test_gzipped_result_same_as_reference(run_build, result, reference):
