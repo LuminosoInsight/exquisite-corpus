b66fdf4e503ae84561363eb00bfdf316fa4b9979
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-05 10:28:06 -0400

    Comment changes

--- scripts/approve-test-data.sh
+++ scripts/approve-test-data.sh
@@ -3,7 +3,7 @@
 input_directories="raw downloaded source-lists"
 output_directories="extracted tokenized messy-counts counts freqs wordfreq"
 
-echo "Copying input files from tests/input_data"
+echo "Copying input directories from tests/input_data"
 for directory in $input_directories; do
     rm -r tests/data/$directory
     cp -r tests/input_data/$directory tests/data
@@ -12,12 +12,14 @@ done
 echo "Running the build"
 XC_BUILD_TEST="1" snakemake -j 4
 
-echo "Copying the output files to tests/reference"
+echo "Copying the output directories to tests/reference"
 rm -r tests/reference/*
 
 for directory in $output_directories; do
     cp -r tests/data/$directory tests/reference
 done
 
-echo "Removing the output files from tests/data"
-rm -r tests/data/*
+echo "Removing the output directories from tests/data"
+for directory in $output_directories; do
+    rm -r tests/data/$directory
+done
