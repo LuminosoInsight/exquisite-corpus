de4d62b274b30a97082679687384a4e718bf0667
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-05 10:19:08 -0400

    Abstract directories in approve-test-data.sh

--- scripts/approve-test-data.sh
+++ scripts/approve-test-data.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
+input_directories="raw downloaded source-lists"
+output_directories="extracted tokenized messy-counts counts freqs wordfreq"
+
 echo "Copying input files from tests/input_data"
-rm -r tests/data/raw
-rm -r tests/data/downloaded
-rm -r tests/data/source-lists
-cp -r tests/input_data/raw tests/data
-cp -r tests/input_data/downloaded tests/data
-cp -r tests/input_data/source-lists tests/data
+for directory in $input_directories; do
+    rm -r tests/data/$directory
+    cp -r tests/input_data/$directory tests/data
+done
 
 echo "Running the build"
 XC_BUILD_TEST="1" snakemake -j 4
 
 echo "Copying the output files to tests/reference"
 rm -r tests/reference/*
-cp -r tests/data/extracted tests/reference
-cp -r tests/data/tokenized tests/reference
-cp -r tests/data/messy-counts tests/reference
-cp -r tests/data/counts tests/reference
-cp -r tests/data/freqs tests/reference
-cp -r tests/data/wordfreq tests/reference
+
+for directory in $output_directories; do
+    cp -r tests/data/$directory tests/reference
+done
 
 echo "Removing the output files from tests/data"
-rm -r tests/data/extracted
-rm -r tests/data/tokenized
-rm -r tests/data/messy-counts
-rm -r tests/data/counts
-rm -r tests/data/freqs
-rm -r tests/data/wordfreq
+rm -r tests/data/*
