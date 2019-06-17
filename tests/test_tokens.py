204b97979313955b9b6a04cf185495c9be478113
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-17 15:31:54 -0400

    Test tokenize_file

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -14,15 +14,39 @@ def run_tokenize(func, test_obj, kwargs):
 
 
 @pytest.mark.parametrize(
-    'text, kwargs, expected',
+    'text, expected, kwargs',
     [
         pytest.param(
-            'This is a sample text in English',
-            {'language': 'en'},
-            'this is a sample text in english\n',
-            id='Basic case'
-        )
+            'This is a sample, &lt;h1&gt; text in English\n\n',
+            'this is a sample h1 text in english\n',
+            {'ftfy': False, 'language': 'en'},
+            id='Tokenize text with simple ftfy fixes'
+        ),
+        pytest.param(
+            'This is a sample text with a mojibake dash â€”',
+            'this is a sample text with a mojibake dash\n',
+            {'ftfy': True, 'language': 'en'},
+            id='Tokenize text will all ftfy fixes'
+        ),
+        pytest.param(
+            'This is text with, some. punctuation;',
+            'this is text with , some . punctuation ;\n',
+            {'punctuation': True, 'language': 'en'},
+            id='Keep punctuation with punctuation kwarg set to True'
+        ),
+        pytest.param(
+            'A text which language is checked',
+            '',
+            {'language': 'cs', 'check_language': True},
+            id='Ignore if a declared language is incorrect'
+        ),
+        pytest.param(
+            'A text which language is checked',
+            'a text which language is checked\n',
+            {'language': 'eng', 'check_language': True},
+            id='Output if a declared language is correct'
+        ),
     ],
 )
-def test_tokenize_file(text, kwargs, expected):
+def test_tokenize_file(text, expected, kwargs):
     assert run_tokenize(tokenize_file, text, kwargs) == expected
