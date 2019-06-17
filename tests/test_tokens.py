12572cf25ccd030961b0b0be7f8b7a8da812d75b
jlowryduda <joanna.teresa.duda@gmail.com>
2019-06-17 11:11:23 -0400

    A basic working test

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,19 +1,28 @@
+import pytest
 from io import StringIO
 
 from exquisite_corpus.tokens import tokenize_file
 
 
-def run_tokenize(func, test_obj):
+def run_tokenize(func, test_obj, kwargs):
     input_file = [test_obj]
     output_file = StringIO()
-    func(input_file, output_file)
+    func(input_file, output_file, **kwargs)
     output_file.seek(0)
     result = output_file.read()
     return result
 
 
-def test_basic_tokenize_file():
-    text = 'This is a sample text in English'
-
-    
-
+@pytest.mark.parametrize(
+    'text, kwargs, expected',
+    [
+        pytest.param(
+            'This is a sample text in English',
+            {'language': 'en'},
+            'this is a sample text in english\n',
+            id='Basic case'
+        )
+    ],
+)
+def test_tokenize_file(text, kwargs, expected):
+    assert run_tokenize(tokenize_file, text, kwargs) == expected
