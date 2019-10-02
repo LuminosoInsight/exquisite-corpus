2aba9ec3c773ae57e83f4a840725ec856bb0f873
jlowryduda <joanna.teresa.duda@gmail.com>
2019-10-02 16:41:31 -0400

    Add tests for tokenize_by_language

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,7 +1,8 @@
 import pytest
 from io import StringIO
+from unittest.mock import mock_open, patch, call
 
-from exquisite_corpus.tokens import tokenize_file
+from exquisite_corpus.tokens import tokenize_file, tokenize_by_language
 
 
 def run_tokenize(func, test_obj, kwargs):
@@ -26,7 +27,7 @@ def run_tokenize(func, test_obj, kwargs):
             'This is a sample text with a mojibake dash â€”',
             'this is a sample text with a mojibake dash\n',
             {'ftfy': True, 'language': 'en'},
-            id='Tokenize text will all ftfy fixes'
+            id='Tokenize text with all ftfy fixes'
         ),
         pytest.param(
             'This is text with, some. punctuation;',
@@ -50,3 +51,36 @@ def run_tokenize(func, test_obj, kwargs):
 )
 def test_tokenize_file(text, expected, kwargs):
     assert run_tokenize(tokenize_file, text, kwargs) == expected
+
+
+@pytest.mark.parametrize(
+    'in_file, expected, kwargs',
+    [
+        pytest.param(
+            ['en\tsample text'],
+            'sample text',
+            {'languages': ['en']},
+            id='Write text to a file for that language'
+        ),
+        pytest.param(
+            ['pl\tsample text'],
+            '',
+            {'languages' : ['en']},
+            id='Ignore if the language of a text is not in a desired language'
+        )
+    ]
+)
+def test_tokenize_by_language(in_file, expected, kwargs):
+    m = mock_open()
+    with patch('builtins.open', m):
+        tokenize_by_language(in_file=in_file, out_dir='.', **kwargs)
+        lang = kwargs['languages'][0]
+        if expected:
+            calls = [call(f'./{lang}.txt', 'w', encoding='utf-8'),
+                 call().write(expected),
+                 call().write('\n'),
+                 call().close()]
+        else:
+            calls = [call(f'./{lang}.txt', 'w', encoding='utf-8'),
+                     call().close()]
+        m.assert_has_calls(calls) # TODO Uninformative error
